6572ed60132960ff46d66678b281cda6afa954e3
ZYXXXT9 <markjasonyoungdelacruz.zyxtii9@gmail.com>
2024-07-14 14:04:48 +0800

    chores: updated portfolio

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,51 @@
 import React from "react";
 
 const Contact = () => {
-  return <div></div>;
+  return (
+    <div
+      name="contact"
+      className="bg-[#0a192f] text-gray-300 w-full min-h-screen flex flex-col justify-center items-center"
+    >
+      <div className="max-w-[1000px] mx-auto p-4  h-full w-full">
+        <form
+          className="flex flex-col max-w-[600px] w-full"
+          action="https://getform.io/f/avrerwwa"
+          method="POST"
+        >
+          <div className="pb-8 text-gray-300">
+            <p className=" text-4xl font-bold border-b-4 inline border-[#2ebfa5] text-gray-300">
+              Contact
+            </p>
+            <p className="py-4 mt-8">Get in touch!</p>
+          </div>
+          <input
+            className="p-4 text-gray-900 outline-none rounded"
+            type="text"
+            name="name"
+            placeholder="Name"
+            required
+          />
+          <input
+            className="my-4 p-4 text-gray-900 outline-none  rounded"
+            type="email"
+            name="email"
+            placeholder="Email"
+            required
+          />
+          <textarea
+            className="p-4 text-gray-900 outline-none rounded"
+            name="message"
+            rows="6"
+            placeholder="Message"
+            required
+          />
+          <button className="group text-white border-2 px-4 py-3 hover:bg-[#2ebfa5] my-8 duration-700 font-bold w-[150px] outline-none">
+            Send Message
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Hero from "./Hero";
+import Navbar from "./Navbar";
 
 const Home = () => {
-  return <div></div>;
+  return (
+    <div name="home" className="flex flex-col w-full h-screen bg-[#0a192f]">
+      <Navbar />
+      <Hero />
+    </div>
+  );
 };
 
 export default Home;
